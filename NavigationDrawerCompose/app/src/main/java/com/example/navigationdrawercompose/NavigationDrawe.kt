f9560e3f3678666248c2e54b78351764f78df0e8
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-02-27 00:04:13 +0700

    add navigation drawer compose

---
+++ NavigationDrawerCompose/app/src/main/java/com/example/navigationdrawercompose/NavigationDrawe.kt
@@ -0,0 +1,140 @@
+package com.example.navigationdrawercompose
+
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationdrawercompose.ui.theme.GreenJc
+import kotlinx.coroutines.launch
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationDrawer(){
+    val navController= rememberNavController()
+    val coroutineScope= rememberCoroutineScope()
+    val drawerState= rememberDrawerState(initialValue = DrawerValue.Closed)
+    val context= LocalContext.current.applicationContext
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerContent = {
+            ModalDrawerSheet {
+                Box(modifier = Modifier
+                    .background(GreenJc)
+                    .fillMaxWidth()
+                    .height(150.dp))
+                {
+                    Text(text = "")
+                }
+                Divider()
+                NavigationDrawerItem(
+                    label = { Text("Home", color = GreenJc) },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate("Home"){
+                            popUpTo(0)
+                        }
+                    },
+                    icon = { Icon(imageVector = Icons.Default.Home, contentDescription = null) },
+                )
+                NavigationDrawerItem(
+                    label = { Text("Favorite", color = GreenJc) },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate("Favorite"){
+                            popUpTo(0)
+                        }
+                    },
+                    icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = null) },
+                )
+                NavigationDrawerItem(
+                    label = { Text("Settings", color = GreenJc) },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate("Settings"){
+                            popUpTo(0)
+                        }
+                    },
+                    icon = { Icon(imageVector = Icons.Default.Settings, contentDescription = null) },
+                )
+            }
+        },
+    ) {
+        Scaffold (
+            topBar = {
+                val coroutineScope = rememberCoroutineScope()
+                TopAppBar(
+                    title = { Text("Wa") },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = GreenJc,
+                        titleContentColor = Color.White,
+                        navigationIconContentColor = Color.White
+                    ),
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                Icons.Rounded.Menu, contentDescription = null
+                            )
+                        }
+                    }
+                )
+            }
+        ) {
+            NavHost(navController=navController, startDestination = "Home"){
+                composable("Home"){
+
+                }
+                composable("Favorite"){
+
+                }
+                composable("Settings"){
+
+                }
+            }
+        }
+    }
+}
---
+++ NavigationDrawerCompose/app/src/main/java/com/example/navigationdrawercompose/model/Drawer.kt
@@ -0,0 +1,5 @@
+package com.example.navigationdrawercompose.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Drawer(var label:String,var icon:ImageVector,var route:String)
